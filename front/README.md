41da01aa78af5e33e077b7f09f67318620e795fe
OOHYEE <azss0470@gmail.com>
2021-11-04 12:14:23 +0900

    Refactor : header UI CSS 변경 + fontAwesome 설치

--- front/README.md
+++ front/README.md
@@ -1,3 +1,7 @@
 # yarn start 클라이언트 실행
 
 ## 실행이 안될 땐 yarn 입력 후 yarn start 입력
+
+## fontawesome
+
+yarn add @fortawesome/fontawesome-free
--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import styles from "./header.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => (
   <header className={styles.header}>
-    <div className={styles.logo}>용병구함</div>
-    <ul className={styles.menu}>
-      <li>내주변</li>
-      <li>긴급구함</li>
-      <li>더보기</li>
-      <li>로그인</li>
-    </ul>
+    <section className={styles.container}>
+      <div className={styles.logo}>용병구함</div>
+      <div className={styles.space}></div>
+      <ul className={styles.menu}>
+        <li>내주변</li>
+        <li>긴급구함</li>
+        <li>더보기</li>
+        <li>로그인</li>
+      </ul>
+      <FontAwesomeIcon icon={faBars} className={styles.toggleBtn} />
+    </section>
   </header>
 );
 
