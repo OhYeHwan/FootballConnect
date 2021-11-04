7a55f486db1b2fea8b7e63a59cba12c89519062a
OOHYEE <azss0470@gmail.com>
2021-11-04 10:44:39 +0900

    Refactor : Header CSS 수정

--- front/src/App.js
+++ front/src/App.js
@@ -1,44 +1,7 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import Notfound from "./components/NotFound";
-//import Main from "./components/Main";
 import Main from "./components/main/main";
-import { Container } from "react-bootstrap";
-import Signup from "./components/Signup";
-import AuthProvider from "./hooks/useAuth";
-import ForgotPassword from "./components/ForgotPassword";
-import UpdateProfile from "./components/UpdateProfile";
-import PrivateRoute from "./components/PrivateRoute";
-import ViewProfile from "./components/View_Profile";
 
 function App() {
-  return (
-    // <Container>
-    //   {/* <div className="w-100"> */}
-    //   <Router>
-    //     <AuthProvider>
-    //       <Navbar />
-
-    //       <Switch>
-    //         <Route exact path="/" component={Main} />
-    //         <PrivateRoute
-    //           exact
-    //           path="/update-profile"
-    //           component={UpdateProfile}
-    //         />
-    //         <Route path="/signup" component={Signup} />
-    //         <Route path="/login" component={Login} />
-    //         <Route path="/forgot-password" component={ForgotPassword} />
-    //         <Route path="/profile" component={ViewProfile} />
-    //         <Route path="*" component={Notfound} />
-    //       </Switch>
-    //     </AuthProvider>
-    //   </Router>
-    //   {/* </div> */}
-    // </Container>
-    <Main />
-  );
+  return <Main />;
 }
 
 export default App;
--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -3,15 +3,13 @@ import styles from "./header.module.css";
 
 const Header = (props) => (
   <header className={styles.header}>
-    <section className={styles.section}>
-      <h1 className={styles.title}>용병구함</h1>
-      <ul className={styles.menu}>
-        <li>내주변</li>
-        <li>긴급구함</li>
-        <li>더보기</li>
-        <li>로그인</li>
-      </ul>
-    </section>
+    <div className={styles.logo}>용병구함</div>
+    <ul className={styles.menu}>
+      <li>내주변</li>
+      <li>긴급구함</li>
+      <li>더보기</li>
+      <li>로그인</li>
+    </ul>
   </header>
 );
 
