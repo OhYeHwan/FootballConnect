ba90cf63336c20db84f4adbabba183906f55edd6
OOHYEE <azss0470@gmail.com>
2021-11-08 10:40:56 +0900

    Style : 헤더 CSS 수정

--- front/src/components/header/header.jsx
+++ front/src/components/header/header.jsx
@@ -6,13 +6,12 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 const Header = (props) => (
   <header className={styles.header}>
     <section className={styles.container}>
-      <div className={styles.logo}>용병구함</div>
-      <div className={styles.space}></div>
+      <h1 className={styles.logo}>용병구함</h1>
       <ul className={styles.menu}>
-        <li>내주변</li>
-        <li>긴급구함</li>
-        <li>더보기</li>
-        <li>로그인</li>
+        <li className={styles.menu_item}>내주변</li>
+        <li className={styles.menu_item}>긴급구함</li>
+        <li className={styles.menu_item}>더보기</li>
+        <li className={styles.menu_item}>로그인</li>
       </ul>
       <FontAwesomeIcon icon={faBars} className={styles.toggleBtn} />
     </section>
